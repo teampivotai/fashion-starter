31fb329ed069890812bdef8ccb2e242bbc88f9d1
Ante Primorac <ante@agilo.co>
2024-10-02 11:57:46 +0200

    add stripe payment provider

--- medusa/medusa-config.js
+++ medusa/medusa-config.js
@@ -1,16 +1,34 @@
-const { loadEnv, defineConfig } = require('@medusajs/utils')
+const { loadEnv, defineConfig, Modules } = require('@medusajs/utils');
 
-loadEnv(process.env.NODE_ENV, process.cwd())
+loadEnv(process.env.NODE_ENV, process.cwd());
 
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
     http: {
       storeCors: process.env.STORE_CORS,
       adminCors: process.env.ADMIN_CORS,
       authCors: process.env.AUTH_CORS,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  }
-})
+      jwtSecret: process.env.JWT_SECRET || 'supersecret',
+      cookieSecret: process.env.COOKIE_SECRET || 'supersecret',
+    },
+  },
+  modules: {
+    [Modules.PAYMENT]: {
+      resolve: '@medusajs/payment',
+      options: {
+        providers: [
+          {
+            id: 'stripe',
+            resolve: '@medusajs/payment-stripe',
+            options: {
+              apiKey: process.env.STRIPE_API_KEY,
+              webhookSecret: process.env.STRIPE_WEBHOOK_SECRET,
+            },
+          },
+        ],
+      },
+    },
+  },
+});
