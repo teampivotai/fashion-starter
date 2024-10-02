bcef7efcc9da63780e651f1af4037b4a1486019a
Ante Primorac <ante@agilo.co>
2024-10-02 18:20:18 +0200

    Refactor Header component to dynamically set text color based on pathName length

--- storefront/src/components/Header.tsx
+++ storefront/src/components/Header.tsx
@@ -14,38 +14,39 @@ interface HeaderProps {
 export const Header: React.FC<HeaderProps> = ({ colorScheme = "black" }) => {
   const pathName = usePathname()
 
+  const pathNameParts = pathName.split("/").filter((part) => part !== "")
+
   return (
     <div
       className={twJoin(
-        "absolute top-0 left-0 w-full text-grayscale-5",
-        colorScheme === "black" &&
-          pathName !== "/cutup" &&
-          "text-grayscale-black",
-        (colorScheme === "white" || pathName === "/cutup") && "text-grayscale-5"
+        "absolute top-0 left-0 w-full",
+        colorScheme === "black" && pathNameParts.length > 1
+          ? "text-grayscale-black"
+          : "text-grayscale-5"
       )}
     >
       <Layout>
         <LayoutColumn className="col-span-full">
           <div className="flex justify-between items-center h-21">
             <h1 className="font-medium text-md">
-              <Link href="/cutup">SofaSocietyCo.</Link>
+              <Link href="/">SofaSocietyCo.</Link>
             </h1>
             <div className="flex items-center gap-8">
-              <Link href="/cutup">About</Link>
-              <Link href="/cutup">Inspiration</Link>
+              <Link href="/cutup/about">About</Link>
+              <Link href="/cutup/inspiration">Inspiration</Link>
               <Link href="/cutup/shop">Shop</Link>
             </div>
             <div className="flex items-center gap-6">
               <Button
                 variant="ghost"
-                colorScheme={pathName === "/cutup" ? "white" : colorScheme}
+                colorScheme={pathNameParts.length < 2 ? "white" : colorScheme}
                 className="p-1"
               >
                 <Icon name="search" className="w-6 h-6" />
               </Button>
               <Button
                 variant="ghost"
-                colorScheme={pathName === "/cutup" ? "white" : colorScheme}
+                colorScheme={pathNameParts.length < 2 ? "white" : colorScheme}
                 className="p-1 uppercase flex"
               >
                 HR
@@ -62,14 +63,14 @@ export const Header: React.FC<HeaderProps> = ({ colorScheme = "black" }) => {
               </Button>
               <Button
                 variant="ghost"
-                colorScheme={pathName === "/cutup" ? "white" : colorScheme}
+                colorScheme={pathNameParts.length < 2 ? "white" : colorScheme}
                 className="p-1"
               >
                 <Icon name="person" className="w-6 h-6" />
               </Button>
               <Button
                 variant="ghost"
-                colorScheme={pathName === "/cutup" ? "white" : colorScheme}
+                colorScheme={pathNameParts.length < 2 ? "white" : colorScheme}
                 className="p-1"
               >
                 <Icon name="case" className="w-6 h-6" />
