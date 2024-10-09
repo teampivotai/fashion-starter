193aacde1d623d58b537ea63746e2a548e68004b
Mateo Juginovic <mateo.juginovic@agilo.co>
2024-10-09 13:41:32 +0200

    added `tablet` design for `Footer` component

--- storefront/src/components/Footer.tsx
+++ storefront/src/components/Footer.tsx
@@ -17,55 +17,57 @@ export const Footer = () => {
     <div className="bg-grayscale-50 py-8 md:py-20">
       <Layout>
         <LayoutColumn className="col-span-13">
-          <div className="flex flex-col justify-between md:gap-20 md:items-center">
-            <div className="max-w-35 md:flex-1 order-2 max-md:mb-9">
-              <h1 className="text-lg md:text-xl mb-2 md:mb-6 leading-[0.9]">
-                Sofa Society Co.
-              </h1>
-              <p className="text-xs">&copy; 2024, Sofa Society</p>
+          <div className="flex max-lg:flex-col justify-between md:gap-20 md:items-center">
+            <div className="flex flex-1 max-lg:w-full max-lg:order-2 max-sm:flex-col justify-between sm:gap-30 lg:gap-20 md:items-center">
+              <div className="max-w-35 md:flex-1 max-md:mb-9">
+                <h1 className="text-lg md:text-xl mb-2 md:mb-6 !leading-[0.9]">
+                  Sofa Society Co.
+                </h1>
+                <p className="text-xs">&copy; 2024, Sofa Society</p>
+              </div>
+              <div className="flex gap-10 xl:gap-18 max-md:text-xs flex-1 justify-between lg:justify-center">
+                <ul className="flex flex-col gap-6 md:gap-4">
+                  <li>
+                    <Link href="/">FAQ</Link>
+                  </li>
+                  <li>
+                    <Link href="/">Help</Link>
+                  </li>
+                  <li>
+                    <Link href="/">Delivery</Link>
+                  </li>
+                  <li>
+                    <Link href="/">Returns</Link>
+                  </li>
+                </ul>
+                <ul className="flex flex-col gap-6 md:gap-4">
+                  <li>
+                    <Link href="/">Instagram</Link>
+                  </li>
+                  <li>
+                    <Link href="/">TikTok</Link>
+                  </li>
+                  <li>
+                    <Link href="/">Pinterest</Link>
+                  </li>
+                  <li>
+                    <Link href="/">Facebook</Link>
+                  </li>
+                </ul>
+                <ul className="flex flex-col gap-6 md:gap-4">
+                  <li>
+                    <Link href="/">Privacy Policy</Link>
+                  </li>
+                  <li>
+                    <Link href="/">Cookie Policy</Link>
+                  </li>
+                  <li>
+                    <Link href="/">Terms of Use</Link>
+                  </li>
+                </ul>
+              </div>
             </div>
-            <div className="flex gap-18 max-md:text-xs flex-1 order-3 justify-center">
-              <ul className="flex flex-col gap-6 md:gap-4">
-                <li>
-                  <Link href="/">FAQ</Link>
-                </li>
-                <li>
-                  <Link href="/">Help</Link>
-                </li>
-                <li>
-                  <Link href="/">Delivery</Link>
-                </li>
-                <li>
-                  <Link href="/">Returns</Link>
-                </li>
-              </ul>
-              <ul className="flex flex-col gap-6 md:gap-4">
-                <li>
-                  <Link href="/">Instagram</Link>
-                </li>
-                <li>
-                  <Link href="/">TikTok</Link>
-                </li>
-                <li>
-                  <Link href="/">Pinterest</Link>
-                </li>
-                <li>
-                  <Link href="/">Facebook</Link>
-                </li>
-              </ul>
-              <ul className="flex flex-col gap-6 md:gap-4">
-                <li>
-                  <Link href="/">Privacy Policy</Link>
-                </li>
-                <li>
-                  <Link href="/">Cookie Policy</Link>
-                </li>
-                <li>
-                  <Link href="/">Terms of Use</Link>
-                </li>
-              </ul>
-            </div>
-            <div className="flex-1 max-w-96 order-1 max-md:mb-16">
+            <div className="flex-1 max-lg:w-full lg:max-w-90 xl:max-w-96 max-lg:order-1 max-md:mb-16">
               <h2 className="text-md md:text-lg mb-2 md:mb-4">
                 Join our newsletter
               </h2>
