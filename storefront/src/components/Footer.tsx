d546c699dee5b81c273c0e46a53fc43e871636d1
Mateo Juginovic <mateo.juginovic@agilo.co>
2024-10-09 12:49:24 +0200

    added `mobile` design for `Footer` component

--- storefront/src/components/Footer.tsx
+++ storefront/src/components/Footer.tsx
@@ -14,16 +14,18 @@ export const Footer = () => {
   })
 
   return (
-    <div className="bg-grayscale-50 py-20">
+    <div className="bg-grayscale-50 py-8 md:py-20">
       <Layout>
         <LayoutColumn className="col-span-13">
-          <div className="flex justify-between gap-20 items-center">
-            <div className="max-w-35 flex-1">
-              <h1 className="text-xl mb-6 leading-[0.9]">Sofa Society Co.</h1>
+          <div className="flex flex-col justify-between md:gap-20 md:items-center">
+            <div className="max-w-35 md:flex-1 order-2 max-md:mb-9">
+              <h1 className="text-lg md:text-xl mb-2 md:mb-6 leading-[0.9]">
+                Sofa Society Co.
+              </h1>
               <p className="text-xs">&copy; 2024, Sofa Society</p>
             </div>
-            <div className="flex gap-18 flex-1 justify-center">
-              <ul className="flex flex-col gap-4">
+            <div className="flex gap-18 max-md:text-xs flex-1 order-3 justify-center">
+              <ul className="flex flex-col gap-6 md:gap-4">
                 <li>
                   <Link href="/">FAQ</Link>
                 </li>
@@ -37,7 +39,7 @@ export const Footer = () => {
                   <Link href="/">Returns</Link>
                 </li>
               </ul>
-              <ul className="flex flex-col gap-4">
+              <ul className="flex flex-col gap-6 md:gap-4">
                 <li>
                   <Link href="/">Instagram</Link>
                 </li>
@@ -51,7 +53,7 @@ export const Footer = () => {
                   <Link href="/">Facebook</Link>
                 </li>
               </ul>
-              <ul className="flex flex-col gap-4">
+              <ul className="flex flex-col gap-6 md:gap-4">
                 <li>
                   <Link href="/">Privacy Policy</Link>
                 </li>
@@ -63,8 +65,10 @@ export const Footer = () => {
                 </li>
               </ul>
             </div>
-            <div className="flex-1 max-w-96">
-              <h2 className="text-lg mb-4">Join our newsletter</h2>
+            <div className="flex-1 max-w-96 order-1 max-md:mb-16">
+              <h2 className="text-md md:text-lg mb-2 md:mb-4">
+                Join our newsletter
+              </h2>
               <p className="mb-4">
                 We will also send you our discount coupons!
               </p>
