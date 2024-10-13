0cd1a215823cdd580de06cf7579dfcdde0d8e3e9
Mateo Juginovic <mateo.juginovic@agilo.co>
2024-10-13 14:45:01 +0200

    added changes to `Checkbox` component

--- storefront/src/components/Checkbox.tsx
+++ storefront/src/components/Checkbox.tsx
@@ -23,11 +23,11 @@ export const Checkbox: React.FC<CheckboxProps> = ({
   <AriaCheckbox.Checkbox
     {...rest}
     className={twMerge(
-      "flex gap-2 items-start text-grayscale-500",
+      "flex gap-2 items-start group text-grayscale-500",
       className as string
     )}
   >
-    <div className="h-4 w-4 mt-1 data-[data-selected]:bg-black data-[data-selected]:text-white transition-colors border border-grayscale-200 hover:border-grayscale-600 flex justify-between items-center">
+    <div className="h-4 w-4 mt-1 group-data-[selected]:bg-black group-data-[selected]:border-black group-hover:group-data-[selected]:bg-grayscale-600 group-hover:group-data-[selected]:border-grayscale-600 text-transparent group-data-[selected]:text-white transition-colors border border-grayscale-200 group-hover:border-grayscale-600 flex justify-between items-center">
       <Icon name="check" className="w-3.5" />
     </div>
     <p {...labelProps}>{label}</p>
