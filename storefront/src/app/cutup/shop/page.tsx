933f6f100872469e45facbe3a164028f7e4aab72
Mateo Juginovic <mateo.juginovic@agilo.co>
2024-10-13 16:35:39 +0200

    added mobile `filter` and `sort` modals

--- storefront/src/app/cutup/shop/page.tsx
+++ storefront/src/app/cutup/shop/page.tsx
@@ -2,6 +2,16 @@
 
 // External components
 import Image from "next/image"
+import {
+  CheckboxGroup,
+  Dialog,
+  DialogTrigger,
+  Label,
+  Modal,
+  ModalOverlay,
+  RadioGroup,
+  Slider,
+} from "react-aria-components"
 
 // Components
 import { Button, Carousel, Layout, LayoutColumn, Link } from "components"
@@ -10,6 +20,19 @@ import { FilterProductsByColor } from "@/components/FilterProductsByColor"
 import { FilterProductsByMaterial } from "@/components/FilterProductsByMaterial"
 import { FilterProductsByCollection } from "@/components/FilterProductsByCollection"
 import { SortProducts } from "@/components/SortProducts"
+import {
+  UiSliderOutput,
+  UiSliderOutputValue,
+  UiSliderThumb,
+  UiSliderTrack,
+} from "@/components/ui/Slider"
+import {
+  UiCheckbox,
+  UiCheckboxBox,
+  UiCheckboxIcon,
+  UiCheckboxLabel,
+} from "@/components/ui/Checkbox"
+import { UiRadio, UiRadioBox, UiRadioLabel } from "@/components/ui/Radio"
 
 export default function ShopPage() {
   return (
@@ -65,20 +88,255 @@ export default function ShopPage() {
         <LayoutColumn>
           <h2 className="text-lg md:text-2xl mb-6 md:mb-8">Shop</h2>
           <div className="flex justify-between gap-10">
-            <Button
-              iconName="plus"
-              iconPosition="end"
-              className="bg-white md:hidden max-md:gap-1 border max-md:text-xs px-3 md:px-4 hover:bg-white border-grayscale-200 h-auto flex-1 grow-0 text-black"
-            >
-              Filter
-            </Button>
+            <DialogTrigger>
+              <Button
+                iconName="plus"
+                iconPosition="end"
+                className="bg-white md:hidden max-md:gap-1 border max-md:text-xs px-3 md:px-4 hover:bg-white border-grayscale-200 h-8 text-black"
+              >
+                Filter
+              </Button>
+              <ModalOverlay
+                isDismissable
+                className="fixed top-0 left-0 w-full h-full bg-black-10%"
+              >
+                <Modal className="bg-white absolute bottom-0 left-0 w-full max-h-full overflow-y-scroll p-6 pb-21">
+                  <Dialog className="focus-visible:outline-none">
+                    {({ close }) => (
+                      <>
+                        <p className="text-md font-semibold mb-6">Price</p>
+                        <Slider
+                          defaultValue={[0, 5000]}
+                          minValue={0}
+                          maxValue={5000}
+                        >
+                          <UiSliderTrack>
+                            {({ state }) =>
+                              state.values.map((_, i) => (
+                                <UiSliderThumb
+                                  key={i}
+                                  index={i}
+                                  aria-label={["start", "end"]?.[i]}
+                                />
+                              ))
+                            }
+                          </UiSliderTrack>
+                          <UiSliderOutput>
+                            {({ state }) =>
+                              state.values.map((_, i) => (
+                                <UiSliderOutputValue key={i}>
+                                  {state.getThumbValueLabel(i)}
+                                </UiSliderOutputValue>
+                              ))
+                            }
+                          </UiSliderOutput>
+                        </Slider>
+                        <hr className="my-6" />
+                        <CheckboxGroup className="flex flex-col">
+                          <Label className="block text-md font-semibold mb-3">
+                            Color
+                          </Label>
+                          <UiCheckbox
+                            value="black"
+                            className="justify-between py-3"
+                          >
+                            <UiCheckboxLabel>Black</UiCheckboxLabel>
+                            <UiCheckboxBox>
+                              <UiCheckboxIcon />
+                            </UiCheckboxBox>
+                          </UiCheckbox>
+                          <UiCheckbox
+                            value="gray"
+                            className="justify-between py-3"
+                          >
+                            <UiCheckboxLabel>Gray</UiCheckboxLabel>
+                            <UiCheckboxBox>
+                              <UiCheckboxIcon />
+                            </UiCheckboxBox>
+                          </UiCheckbox>
+                          <UiCheckbox
+                            value="white"
+                            className="justify-between py-3"
+                          >
+                            <UiCheckboxLabel>White</UiCheckboxLabel>
+                            <UiCheckboxBox>
+                              <UiCheckboxIcon />
+                            </UiCheckboxBox>
+                          </UiCheckbox>
+                          <UiCheckbox
+                            value="red"
+                            className="justify-between py-3"
+                          >
+                            <UiCheckboxLabel>Red</UiCheckboxLabel>
+                            <UiCheckboxBox>
+                              <UiCheckboxIcon />
+                            </UiCheckboxBox>
+                          </UiCheckbox>
+                        </CheckboxGroup>
+                        <hr className="my-6" />
+                        <CheckboxGroup className="flex flex-col">
+                          <Label className="block text-md font-semibold mb-3">
+                            Materials
+                          </Label>
+                          <UiCheckbox
+                            value="velvet"
+                            className="justify-between py-3"
+                          >
+                            <UiCheckboxLabel>Velvet</UiCheckboxLabel>
+                            <UiCheckboxBox>
+                              <UiCheckboxIcon />
+                            </UiCheckboxBox>
+                          </UiCheckbox>
+                          <UiCheckbox
+                            value="linen"
+                            className="justify-between py-3"
+                          >
+                            <UiCheckboxLabel>Linen</UiCheckboxLabel>
+                            <UiCheckboxBox>
+                              <UiCheckboxIcon />
+                            </UiCheckboxBox>
+                          </UiCheckbox>
+                          <UiCheckbox
+                            value="boucle"
+                            className="justify-between py-3"
+                          >
+                            <UiCheckboxLabel>Boucle</UiCheckboxLabel>
+                            <UiCheckboxBox>
+                              <UiCheckboxIcon />
+                            </UiCheckboxBox>
+                          </UiCheckbox>
+                          <UiCheckbox
+                            value="leather"
+                            className="justify-between py-3"
+                          >
+                            <UiCheckboxLabel>Leather</UiCheckboxLabel>
+                            <UiCheckboxBox>
+                              <UiCheckboxIcon />
+                            </UiCheckboxBox>
+                          </UiCheckbox>
+                        </CheckboxGroup>
+                        <hr className="my-6" />
+                        <CheckboxGroup className="flex flex-col mb-5">
+                          <Label className="block text-md font-semibold mb-3">
+                            Collection
+                          </Label>
+                          <UiCheckbox
+                            value="scandinavian-simplicity"
+                            className="justify-between py-3"
+                          >
+                            <UiCheckboxLabel>
+                              Scandinavian Simplicity
+                            </UiCheckboxLabel>
+                            <UiCheckboxBox>
+                              <UiCheckboxIcon />
+                            </UiCheckboxBox>
+                          </UiCheckbox>
+                          <UiCheckbox
+                            value="modern-luxe"
+                            className="justify-between py-3"
+                          >
+                            <UiCheckboxLabel>Modern Luxe</UiCheckboxLabel>
+                            <UiCheckboxBox>
+                              <UiCheckboxIcon />
+                            </UiCheckboxBox>
+                          </UiCheckbox>
+                          <UiCheckbox
+                            value="boho-chic"
+                            className="justify-between py-3"
+                          >
+                            <UiCheckboxLabel>Boho Chic</UiCheckboxLabel>
+                            <UiCheckboxBox>
+                              <UiCheckboxIcon />
+                            </UiCheckboxBox>
+                          </UiCheckbox>
+                          <UiCheckbox
+                            value="timeless-classics"
+                            className="justify-between py-3"
+                          >
+                            <UiCheckboxLabel>Timeless Classics</UiCheckboxLabel>
+                            <UiCheckboxBox>
+                              <UiCheckboxIcon />
+                            </UiCheckboxBox>
+                          </UiCheckbox>
+                        </CheckboxGroup>
+                        <footer className="flex items-center h-21 fixed bottom-0 left-0 w-full bg-white px-6 border-t border-grayscale-100">
+                          <Button isFullWidth onPress={close}>
+                            Show results
+                          </Button>
+                        </footer>
+                      </>
+                    )}
+                  </Dialog>
+                </Modal>
+              </ModalOverlay>
+            </DialogTrigger>
             <div className="flex justify-between gap-4 max-md:hidden">
               <FilterProductsByPrice />
               <FilterProductsByColor />
               <FilterProductsByMaterial />
               <FilterProductsByCollection />
             </div>
-            <SortProducts />
+            <DialogTrigger>
+              <Button
+                iconName="chevron-down"
+                iconPosition="end"
+                className="bg-white md:hidden max-md:gap-1 border max-md:text-xs px-3 md:px-4 hover:bg-white border-grayscale-200 h-8 grow-0 text-black"
+              >
+                Sort by
+              </Button>
+              <ModalOverlay
+                isDismissable
+                className="fixed top-0 left-0 w-full h-full bg-black-10%"
+              >
+                <Modal className="bg-white absolute bottom-0 left-0 w-full max-h-full overflow-y-scroll p-6 pb-21">
+                  <Dialog className="focus-visible:outline-none">
+                    {({ close }) => (
+                      <>
+                        <RadioGroup className="flex flex-col mb-5">
+                          <Label className="block text-md font-semibold mb-3">
+                            Sort by
+                          </Label>
+                          <UiRadio
+                            value="featured"
+                            className="justify-between py-3"
+                          >
+                            <UiRadioLabel>Featured</UiRadioLabel>
+                            <UiRadioBox />
+                          </UiRadio>
+                          <UiRadio
+                            value="best-selling"
+                            className="justify-between py-3"
+                          >
+                            <UiRadioLabel>Best selling</UiRadioLabel>
+                            <UiRadioBox />
+                          </UiRadio>
+                          <UiRadio
+                            value="lowest-price"
+                            className="justify-between py-3"
+                          >
+                            <UiRadioLabel>Lowest price</UiRadioLabel>
+                            <UiRadioBox />
+                          </UiRadio>
+                          <UiRadio
+                            value="highest-price"
+                            className="justify-between py-3"
+                          >
+                            <UiRadioLabel>Highest price</UiRadioLabel>
+                            <UiRadioBox />
+                          </UiRadio>
+                        </RadioGroup>
+                        <footer className="flex items-center h-21 fixed bottom-0 left-0 w-full bg-white px-6 border-t border-grayscale-100">
+                          <Button isFullWidth onPress={close}>
+                            Show results
+                          </Button>
+                        </footer>
+                      </>
+                    )}
+                  </Dialog>
+                </Modal>
+              </ModalOverlay>
+            </DialogTrigger>
+            <SortProducts className="max-md:hidden" />
           </div>
         </LayoutColumn>
       </Layout>
