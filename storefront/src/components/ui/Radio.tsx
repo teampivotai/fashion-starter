6b611621309293274e07f52245e8c1f9e6e6a896
Mateo Juginovic <mateo.juginovic@agilo.co>
2024-10-13 14:16:01 +0200

    added `variant` prop to `Radio` component

--- storefront/src/components/ui/Radio.tsx
+++ storefront/src/components/ui/Radio.tsx
@@ -2,11 +2,21 @@
 import { Radio, RadioProps } from "react-aria-components"
 import { twMerge } from "tailwind-merge"
 
-export const UiRadio: React.FC<RadioProps> = ({ className, ...props }) => (
+type UiRadioOwnProps = {
+  variant?: "ghost" | "outline"
+}
+
+export const UiRadio: React.FC<UiRadioOwnProps & RadioProps> = ({
+  variant = "ghost",
+  className,
+  ...props
+}) => (
   <Radio
     {...props}
     className={twMerge(
       "flex gap-2 group cursor-pointer items-center",
+      variant === "outline" &&
+        "p-4 gap-4 border border-grayscale-200 hover:border-grayscale-400 transition-colors",
       className as string
     )}
   />
@@ -19,7 +29,7 @@ export const UiRadioBox: React.FC<React.ComponentPropsWithoutRef<"span">> = ({
   <span
     {...props}
     className={twMerge(
-      "border border-grayscale-200 w-4 h-4 block rounded-full group-data-[selected]:border-black group-data-[selected]:border-6",
+      "border border-grayscale-200 w-4 h-4 block transition-colors rounded-full group-hover:border-grayscale-400 group-data-[selected]:border-black group-data-[selected]:border-6 group-hover:group-data-[selected]:border-grayscale-600",
       className
     )}
   />
