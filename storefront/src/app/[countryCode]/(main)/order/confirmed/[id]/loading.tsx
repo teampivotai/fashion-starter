6b7e4b0c913e21d09a8213622901ea7a609feefd
Ante Primorac <ante@agilo.co>
2024-10-25 20:23:41 +0200

    Remove order confirmed loading skeleton todo comment

--- storefront/src/app/[countryCode]/(main)/order/confirmed/[id]/loading.tsx
+++ storefront/src/app/[countryCode]/(main)/order/confirmed/[id]/loading.tsx
@@ -1,6 +1,5 @@
 import SkeletonOrderConfirmed from "@modules/skeletons/templates/skeleton-order-confirmed"
 
-// TODO: update the style of the skeleton
 export default function Loading() {
   return <SkeletonOrderConfirmed />
 }
