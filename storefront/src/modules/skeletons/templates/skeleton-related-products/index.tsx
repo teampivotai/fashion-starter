59d1ebce24ccc91aa83940ebbc19c4632247f67d
Mateo Juginovic <mateo.juginovic@agilo.co>
2024-10-25 12:24:54 +0200

    added loading state for product preview component

--- storefront/src/modules/skeletons/templates/skeleton-related-products/index.tsx
+++ storefront/src/modules/skeletons/templates/skeleton-related-products/index.tsx
@@ -1,24 +1,28 @@
+// Utilities
 import repeat from "@lib/util/repeat"
+
+// Components
 import SkeletonProductPreview from "@modules/skeletons/components/skeleton-product-preview"
+import { Layout, LayoutColumn } from "@/components/Layout"
 
 const SkeletonRelatedProducts = () => {
   return (
-    <div className="product-page-constraint">
-      <div className="flex flex-col gap-8 items-center text-center mb-8">
-        <div className="w-20 h-6 animate-pulse bg-gray-100"></div>
-        <div className="flex flex-col gap-4 items-center text-center mb-16">
-          <div className="w-96 h-10 animate-pulse bg-gray-100"></div>
-          <div className="w-48 h-10 animate-pulse bg-gray-100"></div>
-        </div>
-      </div>
-      <ul className="grid grid-cols-2 small:grid-cols-3 medium:grid-cols-4 gap-x-6 gap-y-8 flex-1">
+    <>
+      <Layout>
+        <LayoutColumn className="mt-26 md:mt-36">
+          <h4 className="text-lg md:text-2xl mb-8 md:mb-16">
+            Related products
+          </h4>
+        </LayoutColumn>
+      </Layout>
+      <Layout className="gap-y-10">
         {repeat(3).map((index) => (
-          <li key={index}>
+          <LayoutColumn className="!col-span-6 md:!col-span-4" key={index}>
             <SkeletonProductPreview />
-          </li>
+          </LayoutColumn>
         ))}
-      </ul>
-    </div>
+      </Layout>
+    </>
   )
 }
 
