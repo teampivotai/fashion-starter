63e38c52c327025d7bf50c1ada46d8165bec04ad
Ante Primorac <ante@agilo.co>
2024-10-29 13:34:18 +0100

    Fix collections fields filter

--- storefront/src/lib/data/collections.ts
+++ storefront/src/lib/data/collections.ts
@@ -12,11 +12,11 @@ export const retrieveCollection = cache(async function (id: string) {
 export const getCollectionsList = cache(async function (
   offset: number = 0,
   limit: number = 100,
-  fields: (keyof HttpTypes.StoreCollection)[] = []
+  fields?: (keyof HttpTypes.StoreCollection)[]
 ): Promise<{ collections: HttpTypes.StoreCollection[]; count: number }> {
   return sdk.store.collection
     .list(
-      { limit, offset, fields: fields.join(",") },
+      { limit, offset, fields: fields ? fields.join(",") : undefined },
       { next: { tags: ["collections"] } }
     )
     .then(({ collections }) => ({ collections, count: collections.length }))
@@ -24,11 +24,11 @@ export const getCollectionsList = cache(async function (
 
 export const getCollectionByHandle = cache(async function (
   handle: string,
-  fields: (keyof HttpTypes.StoreCollection)[] = []
+  fields?: (keyof HttpTypes.StoreCollection)[]
 ): Promise<HttpTypes.StoreCollection> {
   return sdk.store.collection
     .list(
-      { handle, fields: fields.join(","), limit: 1 },
+      { handle, fields: fields ? fields.join(",") : undefined, limit: 1 },
       { next: { tags: ["collections"] } }
     )
     .then(({ collections }) => collections[0])
