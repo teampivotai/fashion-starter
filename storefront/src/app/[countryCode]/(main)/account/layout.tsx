35a890cf10f018382db6c854e0882885739eeb11
Mateo Juginovic <mateo.juginovic@agilo.co>
2024-11-12 10:59:46 +0100

    added changes to spacing and removed `Log out` button

--- storefront/src/app/[countryCode]/(main)/account/layout.tsx
+++ storefront/src/app/[countryCode]/(main)/account/layout.tsx
@@ -16,7 +16,7 @@ export default function AccountLayout(props: { children: React.ReactNode }) {
 
   return (
     <div className="flex max-md:flex-col">
-      <div className="sticky left-0 shrink-0 top-0 md:max-w-75 lg:max-w-93 py-8 max-md:mt-18 max-md:top-18 md:pt-45 md:pb-9 max-md:border-b max-md:border-grayscale-200 max-md:overflow-x-auto bg-white md:bg-grayscale-50 w-full h-21 md:h-screen z-10">
+      <div className="sticky left-0 shrink-0 z-30 top-0 md:max-w-75 lg:max-w-93 py-8 max-md:mt-18 max-md:top-18 md:pt-45 md:pb-9 max-md:border-b max-md:border-grayscale-200 max-md:overflow-x-auto bg-white md:bg-grayscale-50 w-full h-21 md:h-screen">
         <div className="md:max-w-54 mx-auto flex max-md:items-center md:flex-col md:justify-between h-full max-md:px-4 max-md:container max-md:mx-auto">
           <div className="max-md:flex max-md:gap-22">
             <h1 className="text-lg mb-16 max-md:hidden">My account</h1>
@@ -48,14 +48,8 @@ export default function AccountLayout(props: { children: React.ReactNode }) {
           </Button>
         </div>
       </div>
-      <div className="max-md:px-4 overflow-hidden max-md:container max-md:mx-auto md:px-10 pt-8 md:pt-45 pb-36 w-full lg:max-w-200 xl:mx-auto 2xl:ml-30">
+      <div className="max-md:px-4 overflow-hidden max-md:container max-md:mx-auto md:px-10 pt-8 md:pt-45 pb-26 md:pb-36 w-full lg:max-w-200 xl:mx-auto 2xl:ml-30">
         {props.children}
-        <div className="mt-16 md:hidden">
-          <p className="text-md mb-6">Log out</p>
-          <Button variant="outline" isFullWidth>
-            Log out
-          </Button>
-        </div>
       </div>
     </div>
   )
