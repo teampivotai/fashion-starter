5ce7d34f426ba4d99ad299652f1bd2743d2b756a
Ante Primorac <ante@agilo.co>
2024-11-13 14:18:59 +0100

    Update image sizes in ImageGallery component for better responsiveness

--- storefront/src/modules/products/components/image-gallery/index.tsx
+++ storefront/src/modules/products/components/image-gallery/index.tsx
@@ -27,7 +27,7 @@ const ImageGallery = ({ images, className }: ImageGalleryProps) => {
             priority={index <= 2 ? true : false}
             alt={`Product image ${index + 1}`}
             fill
-            sizes="(max-width: 576px) 575px, (max-width: 768px) 767px, (max-width: 1024px) 589px, 456px"
+            sizes="(max-width: 768px) 100vw, (max-width: 1024px) 589px, (max-width: 1279px) 384px, 456px"
             className="object-cover"
           />
         </div>
