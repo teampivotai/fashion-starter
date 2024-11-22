849bb4bc05c48e45b7d14aad742a661581316061
Mateo Juginovic <mateo.juginovic@agilo.co>
2024-11-22 13:26:41 +0100

    added changes to `email templates`

--- medusa/src/modules/resend/emails/auth-password-reset.tsx
+++ medusa/src/modules/resend/emails/auth-password-reset.tsx
@@ -1,59 +1,48 @@
-import * as React from 'react';
-import {
-  Tailwind,
-  Text,
-  Container,
-  Heading,
-  Html,
-  Button,
-} from '@react-email/components';
-import { CustomerDTO, HttpTypes } from '@medusajs/framework/types';
+// External components
+import { Text, Heading, Button } from "@react-email/components";
+
+// Types
+import { CustomerDTO } from "@medusajs/framework/types";
+
+// Components
+import EmailLayout from "./components/EmailLayout";
 
 type Props = {
-  customer: Pick<CustomerDTO, 'id' | 'email' | 'first_name' | 'last_name'>;
+  customer: Pick<CustomerDTO, "id" | "email" | "first_name" | "last_name">;
   token: string;
 };
 
 export default function AuthPasswordResetEmail({ customer, token }: Props) {
   return (
-    <Html>
-      <Tailwind>
-        <Container>
-          <Heading>Reset your password</Heading>
-
-          <Text>Hi {customer.first_name},</Text>
-
-          <Text>
-            We received a request to reset your Sofa Society account password.
-            Click below to set a new password:
-          </Text>
-
-          <Button
-            href={`${
-              process.env.STOREFRONT_URL
-            }/auth/reset-password?email=${encodeURIComponent(
-              customer.email,
-            )}&token=${encodeURIComponent(token)}`}
-          >
-            Reset password
-          </Button>
-
-          <Text>
-            If you didn’t request this change, please ignore this email, and
-            your current password will remain unchanged.
-          </Text>
-        </Container>
-      </Tailwind>
-    </Html>
+    <EmailLayout>
+      <Heading className="text-2xl mt-0 mb-10">Reset your password</Heading>
+      <Text className="text-md !mb-10">
+        We received a request to reset your Sofa Society account password. Click
+        below to set a new password:
+      </Text>
+      <Button
+        href={`${
+          process.env.STOREFRONT_URL
+        }/auth/reset-password?email=${encodeURIComponent(
+          customer.email
+        )}&token=${encodeURIComponent(token)}`}
+        className="inline-flex items-center focus-visible:outline-none rounded-xs justify-center transition-colors bg-black hover:bg-grayscale-500 text-white h-10 px-6 mb-10">
+        Reset password
+      </Button>
+      <Text className="text-md text-grayscale-500 m-0">
+        If you didn&apos;t request this change, please ignore this email, and
+        your current password will remain unchanged.
+      </Text>
+    </EmailLayout>
   );
 }
 
 AuthPasswordResetEmail.PreviewProps = {
   customer: {
-    id: '1',
-    email: 'example@medusa.local',
-    first_name: 'John',
-    last_name: 'Doe',
+    id: "1",
+    email: "example@medusa.local",
+    first_name: "John",
+    last_name: "Doe",
   },
-  token: '1234567789012345677890',
+  token: "1234567789012345677890",
 } satisfies Props;
