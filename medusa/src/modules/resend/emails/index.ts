202a7a56863cd81b81e61984645d2a464432e533
Ante Primorac <ante@agilo.co>
2024-11-27 13:32:53 +0100

    add TODO comment to support notification data in email subjects

--- medusa/src/modules/resend/emails/index.ts
+++ medusa/src/modules/resend/emails/index.ts
@@ -2,6 +2,7 @@ import AuthPasswordResetEmail from './auth-password-reset';
 import OrderPlacedEmail from './order-placed';
 import WelcomeEmail from './welcome';
 
+// TODO: we should be able to use notification data in subjects too
 export const subjects = {
   'auth-password-reset': 'Reset your password',
   'order-placed': 'Your order has been placed',
