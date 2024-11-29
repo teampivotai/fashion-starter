0415b47c93e9a7f53f07c22e68583265a0a0504b
Mateo Juginovic <mateo.juginovic@agilo.co>
2024-11-29 15:22:00 +0100

    added `colorScheme` and `position` props to `Drawer` component

--- storefront/src/components/Drawer.tsx
+++ storefront/src/components/Drawer.tsx
@@ -6,27 +6,47 @@ import { twJoin, twMerge } from "tailwind-merge"
 import { Icon } from "@/components/Icon"
 
 export interface DrawerProps extends React.ComponentPropsWithoutRef<"div"> {
+  colorScheme?: "light" | "dark"
+  position?: "left" | "right"
   isOpened?: boolean
   onCloseClick?: () => void
   onBackdropClick?: () => void
+  closeButtonClassName?: string
 }
 
 export const Drawer: React.FC<DrawerProps> = ({
-  children,
+  colorScheme = "dark",
+  position = "left",
   isOpened,
   onCloseClick,
   onBackdropClick,
+  closeButtonClassName,
+  className,
+  children,
   ...rest
 }) => {
+  const baseClasses =
+    "flex-col items-center top-0 z-50 fixed overflow-y-scroll justify-self-center w-full max-h-screen h-screen max-w-75"
+  const colorSchemeClasses =
+    colorScheme === "light" ? "bg-white text-black" : "bg-black text-white"
+  const positionClasses =
+    position === "left"
+      ? isOpened
+        ? "opacity-100 visible translate-x-0 left-0"
+        : "opacity-0 invisible -translate-x-full left-0"
+      : isOpened
+        ? "opacity-100 visible translate-x-0 right-0"
+        : "opacity-0 invisible translate-x-full right-0"
+
   return (
     <>
       <div
         {...rest}
         className={twMerge(
-          "flex-col items-center bg-black top-0 left-0 z-50 fixed justify-self-center w-full max-h-screen h-screen max-w-75",
-          isOpened
-            ? "opacity-100 visible translate-x-0"
-            : "opacity-0 invisible -translate-x-full"
+          baseClasses,
+          colorSchemeClasses,
+          positionClasses,
+          className
         )}
         style={{
           transition: isOpened
@@ -34,10 +54,20 @@ export const Drawer: React.FC<DrawerProps> = ({
             : "transform 300ms, opacity 200ms, visibility 100ms",
         }}
       >
-        <button className="absolute top-6 right-8" onClick={onCloseClick}>
-          <Icon name="close" className="w-6 text-white" />
+        <button
+          className={twMerge("absolute top-6 right-8", closeButtonClassName)}
+          onClick={onCloseClick}
+        >
+          <Icon
+            name="close"
+            className={twJoin(
+              "w-6 text-white",
+              colorScheme === "light" && "text-black",
+              colorScheme === "dark" && "text-white"
+            )}
+          />
         </button>
-        <div className="w-full overflow-y-scroll h-full">{children}</div>
+        {children}
       </div>
       <div
         onClick={onBackdropClick}
