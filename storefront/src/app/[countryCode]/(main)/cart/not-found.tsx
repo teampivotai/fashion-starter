740b8f2c605deb49941a6bc71fdcfa84de5cbcf1
mateo-agilo <mateo.juginovic@agilo.co>
2025-01-17 11:08:01 +0100

    Changed `NotFound` pages to use a centralized `NotFoundPage` component for improved consistency and maintainability

--- storefront/src/app/[countryCode]/(main)/cart/not-found.tsx
+++ storefront/src/app/[countryCode]/(main)/cart/not-found.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from "next"
 
-import { LocalizedLink } from "@/components/LocalizedLink"
+import NotFoundPage from "app/not-found"
 
 export const metadata: Metadata = {
   title: "404",
@@ -8,14 +8,5 @@ export const metadata: Metadata = {
 }
 
 export default function NotFound() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-64px)]">
-      <h1 className="text-2xl-semi text-ui-fg-base">Page not found</h1>
-      <p className="text-small-regular text-ui-fg-base">
-        The cart you tried to access does not exist. Clear your cookies and try
-        again.
-      </p>
-      <LocalizedLink href="/">Go to frontpage</LocalizedLink>
-    </div>
-  )
+  return <NotFoundPage />
 }
