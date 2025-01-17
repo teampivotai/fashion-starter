f37c6e5477a94871771918fc8f740e4f1e72fc64
mateo-agilo <mateo.juginovic@agilo.co>
2025-01-17 15:28:21 +0100

    Update `AccountLayout` component for improved responsiveness with Tailwind CSS

--- storefront/src/app/[countryCode]/(main)/account/layout.tsx
+++ storefront/src/app/[countryCode]/(main)/account/layout.tsx
@@ -7,7 +7,7 @@ export default function AccountLayout(props: { children: React.ReactNode }) {
   return (
     <div className="flex max-md:flex-col">
       <div className="sticky left-0 shrink-0 z-30 top-0 md:max-w-75 lg:max-w-93 py-2 max-md:mt-18 max-md:top-18 md:pt-45 md:pb-9 max-md:border-b max-md:border-grayscale-200 max-md:overflow-x-auto bg-white md:bg-grayscale-50 w-full md:h-screen">
-        <div className="md:max-w-54 mx-auto flex max-md:items-center md:flex-col md:justify-between h-full max-md:px-4 max-md:container max-md:mx-auto">
+        <div className="md:max-w-54 mx-auto flex max-md:items-center md:flex-col md:justify-between h-full max-md:px-4 max-md:sm:container max-md:mx-auto">
           <div className="max-md:flex max-md:gap-22">
             <h1 className="text-lg mb-14 max-md:hidden">My account</h1>
             <SidebarNav />
@@ -20,7 +20,7 @@ export default function AccountLayout(props: { children: React.ReactNode }) {
           </SignOutButton>
         </div>
       </div>
-      <div className="max-md:px-4 overflow-hidden max-md:container max-md:mx-auto md:px-10 pt-10 md:pt-45 pb-26 md:pb-36 w-full lg:max-w-200 xl:mx-auto 2xl:ml-30">
+      <div className="max-md:px-4 overflow-hidden max-md:sm:container max-md:mx-auto md:px-10 pt-10 md:pt-45 pb-26 md:pb-36 w-full lg:max-w-200 xl:mx-auto 2xl:ml-30">
         {props.children}
       </div>
     </div>
