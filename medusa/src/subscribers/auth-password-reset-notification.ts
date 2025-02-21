295d13f06fdf52e4f674e5121b11916781c63766
delicloredana <loredana.delic@agilo.co>
2025-02-21 14:26:49 +0100

    fix: ensure correct subscriber data props during password reset

--- medusa/src/subscribers/auth-password-reset-notification.ts
+++ medusa/src/subscribers/auth-password-reset-notification.ts
@@ -1,37 +1,36 @@
-import type { SubscriberArgs, SubscriberConfig } from '@medusajs/medusa';
-import { ContainerRegistrationKeys, Modules } from '@medusajs/framework/utils';
-import type { CustomerDTO } from '@medusajs/framework/types';
+import type { SubscriberArgs, SubscriberConfig } from "@medusajs/medusa";
+import { ContainerRegistrationKeys, Modules } from "@medusajs/framework/utils";
+import type { CustomerDTO } from "@medusajs/framework/types";
 
 export default async function sendPasswordResetNotification({
   event: { data },
   container,
-}: SubscriberArgs<{ entityId: string; token: string }>) {
+}: SubscriberArgs<{ entity_id: string; token: string }>) {
   const query = container.resolve(ContainerRegistrationKeys.QUERY);
   const notificationModuleService = container.resolve(Modules.NOTIFICATION);
 
   const fields = [
-    'id',
-    'email',
-    'first_name',
-    'last_name',
+    "id",
+    "email",
+    "first_name",
+    "last_name",
   ] as const satisfies (keyof CustomerDTO)[];
 
   const { data: customers } = await query.graph({
-    entity: 'customer',
+    entity: "customer",
     fields,
-    filters: { id: data.entityId },
+    filters: { email: data.entity_id },
   });
-
   const customer = customers[0] as Pick<CustomerDTO, (typeof fields)[number]>;
 
   await notificationModuleService.createNotifications({
     to: customer.email,
-    channel: 'email',
-    template: 'auth-password-reset',
+    channel: "email",
+    template: "auth-password-reset",
     data: { customer, token: data.token },
   });
 }
 
 export const config: SubscriberConfig = {
-  event: 'auth.password_reset',
+  event: "auth.password_reset",
 };
