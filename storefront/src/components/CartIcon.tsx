b00aa6279be4573e38636bb29dc6c54b73e6e0bb
delicloredana <loredana.delic@agilo.co>
2025-03-14 10:02:05 +0100

    revert cart icon changes

--- storefront/src/components/CartIcon.tsx
+++ storefront/src/components/CartIcon.tsx
@@ -1,17 +1,23 @@
-"use client"
+import { Suspense } from "react"
+import { getCartQuantity } from "@lib/data/cart"
 import { Icon, IconProps } from "@/components/Icon"
-import { withReactQueryProvider } from "@lib/util/react-query"
-import { useCartQuantity } from "hooks/cart"
 
-export const CartIcon = withReactQueryProvider<
+const CartIconWithQuantity: React.FC<
   Omit<IconProps, "status" | "name">
->((props) => {
-  const { data: quantity } = useCartQuantity()
+> = async (props) => {
+  const quantity = await getCartQuantity()
+
   return (
-    <Icon
-      name="case"
-      status={quantity && quantity > 0 ? quantity : undefined}
-      {...props}
-    />
+    <Icon name="case" status={quantity > 0 ? quantity : undefined} {...props} />
   )
-})
+}
+
+export const CartIcon: React.FC<Omit<IconProps, "status" | "name">> = (
+  props
+) => {
+  return (
+    <Suspense fallback={<Icon name="case" {...props} />}>
+      <CartIconWithQuantity {...props} />
+    </Suspense>
+  )
+}
