e9095560e6de8f50c1b81d32894b136c6ae9928d
delicloredana <loredana.delic@agilo.co>
2025-03-20 09:07:35 +0100

    feat: cache collection fetching

--- storefront/src/lib/data/collections.ts
+++ storefront/src/lib/data/collections.ts
@@ -1,70 +1,84 @@
 import { sdk } from "@lib/config"
-import { cache } from "react"
 import { getProductsList } from "@lib/data/products"
 import { HttpTypes } from "@medusajs/types"
 
-export const retrieveCollection = cache(async function (id: string) {
-  return sdk.store.collection
-    .retrieve(id, {}, { next: { tags: ["collections"] } })
+export const retrieveCollection = async function (id: string) {
+  return sdk.client
+    .fetch<{ collection: HttpTypes.StoreCollection }>(
+      `/store/collections/${id}`,
+      {
+        next: { tags: ["collections"] },
+        cache: "force-cache",
+      }
+    )
     .then(({ collection }) => collection)
-})
+}
 
-export const getCollectionsList = cache(async function (
+export const getCollectionsList = async function (
   offset: number = 0,
   limit: number = 100,
   fields?: (keyof HttpTypes.StoreCollection)[]
 ): Promise<{ collections: HttpTypes.StoreCollection[]; count: number }> {
-  return sdk.store.collection
-    .list(
-      { limit, offset, fields: fields ? fields.join(",") : undefined },
-      { next: { tags: ["collections"] } }
-    )
+  return sdk.client
+    .fetch<{
+      collections: HttpTypes.StoreCollection[]
+      count: number
+    }>("/store/collections", {
+      query: { limit, offset, fields: fields ? fields.join(",") : undefined },
+      next: { tags: ["collections"] },
+      cache: "force-cache",
+    })
     .then(({ collections }) => ({ collections, count: collections.length }))
-})
+}
 
-export const getCollectionByHandle = cache(async function (
+export const getCollectionByHandle = async function (
   handle: string,
   fields?: (keyof HttpTypes.StoreCollection)[]
 ): Promise<HttpTypes.StoreCollection> {
-  return sdk.store.collection
-    .list(
-      { handle, fields: fields ? fields.join(",") : undefined, limit: 1 },
-      { next: { tags: ["collections"] } }
-    )
+  return sdk.client
+    .fetch<HttpTypes.StoreCollectionListResponse>(`/store/collections`, {
+      query: {
+        handle,
+        fields: fields ? fields.join(",") : undefined,
+        limit: 1,
+      },
+      next: { tags: ["collections"] },
+      cache: "force-cache",
+    })
     .then(({ collections }) => collections[0])
-})
+}
 
-export const getCollectionsWithProducts = cache(
-  async (countryCode: string): Promise<HttpTypes.StoreCollection[] | null> => {
-    const { collections } = await getCollectionsList(0, 3)
+export const getCollectionsWithProducts = async (
+  countryCode: string
+): Promise<HttpTypes.StoreCollection[] | null> => {
+  const { collections } = await getCollectionsList(0, 3)
 
-    if (!collections) {
-      return null
-    }
-
-    const collectionIds = collections
-      .map((collection) => collection.id)
-      .filter(Boolean) as string[]
-
-    const { response } = await getProductsList({
-      queryParams: { collection_id: collectionIds },
-      countryCode,
-    })
-
-    response.products.forEach((product) => {
-      const collection = collections.find(
-        (collection) => collection.id === product.collection_id
-      )
-
-      if (collection) {
-        if (!collection.products) {
-          collection.products = []
-        }
-
-        collection.products.push(product)
-      }
-    })
-
-    return collections as unknown as HttpTypes.StoreCollection[]
+  if (!collections) {
+    return null
   }
-)
+
+  const collectionIds = collections
+    .map((collection) => collection.id)
+    .filter(Boolean) as string[]
+
+  const { response } = await getProductsList({
+    queryParams: { collection_id: collectionIds },
+    countryCode,
+  })
+
+  response.products.forEach((product) => {
+    const collection = collections.find(
+      (collection) => collection.id === product.collection_id
+    )
+
+    if (collection) {
+      if (!collection.products) {
+        collection.products = []
+      }
+
+      collection.products.push(product)
+    }
+  })
+
+  return collections as unknown as HttpTypes.StoreCollection[]
+}
