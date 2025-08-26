f8b27c1a759daa2041c74e18d664202d2135775c
Ante Primorac <ante@agilo.com>
2025-08-26 11:13:53 +0200

    feat(config): add jwt expiration setting to configuration

--- medusa/medusa-config.js
+++ medusa/medusa-config.js
@@ -17,6 +17,7 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS,
       jwtSecret: process.env.JWT_SECRET || 'supersecret',
       cookieSecret: process.env.COOKIE_SECRET || 'supersecret',
+      jwtExpiresIn: process.env.JWT_EXPIRES_IN || '24h',
     },
   },
   modules: [
